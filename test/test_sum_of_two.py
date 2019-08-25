9952b7af4b0cfd3bd015d117ec01eac175c2d082
techrabbit58 <dzb@pruefbit.de>
2019-08-25 18:56:18 +0200

    bug fixes

--- test/test_sum_of_two.py
+++ test/test_sum_of_two.py
@@ -13,7 +13,8 @@ targets = [
     (100, False),
     (5, True),
     (0, True),
-    (1, False),
+    (1, True),
+    (3, False),
     (10, True),
     (14, True),
     (9, True),
@@ -25,6 +26,18 @@ targets = [
 def test_sum_of_two(numbers, target, outcome):
     check = SumOfTwo(numbers, target)
     all_checks = [check.naive(), check.iterative(), check.memoizing()]
-    assert all([lambda x: x == outcome for x in all_checks])
+    assert all(all_checks) == outcome
+
+
+@pytest.mark.parametrize('targets', targets)
+def test_sum_of_two_empty_numbers_list(targets):
+    check = SumOfTwo([], targets[0])
+    all_checks = [check.naive(), check.iterative(), check.memoizing()]
+    assert not any(all_checks)
+
+
+def test_sum_of_two_numbers_are_none():
+    with pytest.raises(TypeError):
+        check = SumOfTwo(None, 0).naive()
 
 # last line of code
