8c335f754d26ce447b8b2a93777895bd5d4b7b0d
techrabbit58 <dzb@pruefbit.de>
2019-08-25 13:35:35 +0200

    new tests

--- test/test_sum_of_two.py
+++ test/test_sum_of_two.py
@@ -1,3 +1,11 @@
 """Test the implementation of the common algorithm 'SumOfTwo'."""
+from common_algorithms import SumOfTwo
+
+
+def test_sum_of_two():
+    numbers = [-2, 3, 4, 11, 2, 7]
+    target = 0
+    check = SumOfTwo(numbers, target)
+    assert all([check.naive(), check.iterative(), check.memoizing()])
 
 # last line of code
